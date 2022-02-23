b0b1818690873e185278ef8288175014e0808f14
“ehsan-elgendi” <ehsan.elgendi@gmail.com>
2022-02-22 21:43:23 -0500

    Adding react ecommerce app

---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItem = [
+    {
+        id: 1,
+        img: "/img/pexels-teona-swift-6912895.jpg",
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: "/img/pexels-bulut-tuncay-10786112.jpg",
+        title: "AUTUMN COLLECTION",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fcf1ed",
+    },
+    {
+        id: 3,
+        img: "/img/pexels-fahd-ahmed-salem-9456561.jpg",
+        title: "POPULAR SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fbf0f4",
+    },
+];
+
+export const categories = [
+    {
+        id: 1,
+        img: "/img/cat-1.jpg",
+        title: "SHIRT STYLE!",
+    },
+    {
+        id: 2,
+        img: "/img/cat-2.jpg",
+        title: "LOUNGEWEAR LOVE",
+    },
+    {
+        id: 3,
+        img: "/img/cat-3.jpg",
+        title: "LIGHT JACKETS",
+    },
+];
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: "/img/pro1.png",
+    },
+    {
+        id: 2,
+        img: "/img/pro1.png",
+    },
+    {
+        id: 3,
+        img: "/img/pro1.png",
+    },
+    {
+        id: 4,
+        img: "/img/pro1.png",
+    },
+    {
+        id: 5,
+        img: "/img/pro1.png",
+    },
+    {
+        id: 6,
+        img: "/img/pro1.png",
+    },
+    {
+        id:7,
+        img:"/img/pro1.png",
+    },
+    {
+        id:8,
+        img:"/img/pro1.png",
+    },
+];
